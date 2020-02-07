b4b2bd586a68d9a678e15799913cfab9203f42b6
Deni <denizakya@gmail.com>
2020-02-07 16:05:02 +0700

    Use heap sorted

--- Sources/App/Controllers/SortController.swift
+++ Sources/App/Controllers/SortController.swift
@@ -5,6 +5,7 @@ final class SortController {
     
     func index(_ req: Request) throws -> Future<[Int]> {
         let items = (1...1000).shuffled()
-        return req.future(mergeSorted(array: items))
+        return req.future(heapSorted(items: items))
     }
 }
+
