a0f7a97bf573db243e467b9abb7b17dfb52fd5e4
Deni <denizakya@gmail.com>
2020-02-08 13:36:23 +0700

    Return HTTP ok when request for health

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,16 +1,14 @@
 import Vapor
-import lib
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
+    
     router.get { req in
         "It works!"
     }
-    
-    // Basic "Hello, world!" example
+
     router.get("health") { req in
-        "OK"
+        return req.future(HTTPStatus.ok)
     }
 
     let sortController = SortController()
